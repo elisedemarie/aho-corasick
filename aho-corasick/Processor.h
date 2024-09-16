647c3d3c94091064cad3b95c151c6ac372df39fe
Elise <135685125+elisedemarie@users.noreply.github.com>
2024-09-16 11:16:20 +1000

    Update Processor.h

--- aho-corasick/Processor.h
+++ aho-corasick/Processor.h
@@ -9,21 +9,23 @@
 using namespace std;
 
 struct Match {
-    string matchedText;
+    const string* matchedText;
     string fileName;
 };
 
 class Processor {
     private:
         Trie* trie;
+        vector<Match> matches;
     public:
         void addKeywordsFromSet(unordered_set<string> keywords);
         void addKeywordsFromFile(string filePath);
         void addKeywordsFromVector(vector<string> keywords);
-        vector<string> searchTextFromString(string text);
-        vector<Match> searchTextFromFile(string filePath);
-        vector<Match> searchMultipleFiles(string listFilePath);
-        void writeResults(vector<Match> results, string fileName);
+        vector<string*> searchTextFromString(string text);
+        void searchTextFromFile(string filePath);
+        void searchMultipleFiles(string listFilePath);
+        void writeResults(string fileName);
+        int countMatches();
 };
 
 
